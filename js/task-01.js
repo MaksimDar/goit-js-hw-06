706bf4e83eb6ecff481f45b9a5bd689c40e78a3f
Maksim1903 <dovgusha.m@gmail.com>
2022-11-16 21:08:24 +0100

    3 tasks

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,8 @@
-const list = documents.quarySelector("#categories");
+const numberOfCategories = document.querySelector("#categories");
+console.log("Number of categories:", numberOfCategories.children.length);
+const items = document.querySelectorAll(".item");
+items.forEach((element) => {
+  console.log("Category:", element.firstElementChild.textContent);
+  const elementsInCategory = element.querySelectorAll("li");
+  console.log("Elements:", elementsInCategory.length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,36 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const items = document.querySelector("#ingredients");
+items.classList.add("item");
+const firstIngredient = document.createElement("li");
+firstIngredient.textContent = "Potatoes";
+
+const secondIngredient = document.createElement("li");
+secondIngredient.textContent = "Mushrooms";
+
+const thirdIngredient = document.createElement("li");
+thirdIngredient.textContent = "Garlic";
+
+const fourhIngredient = document.createElement("li");
+fourhIngredient.textContent = "Tomatoes";
+
+const fifthIngredient = document.createElement("li");
+fifthIngredient.textContent = "Herbs";
+
+const sixthIngredient = document.createElement("li");
+sixthIngredient.textContent = "Condiments";
+
+items.prepend(
+  firstIngredient,
+  secondIngredient,
+  thirdIngredient,
+  fourhIngredient,
+  fifthIngredient,
+  sixthIngredient
+);
