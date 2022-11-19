e531bc8fb4c0c39dc62ea0c4004efbb13194598a
Maksim1903 <dovgusha.m@gmail.com>
2022-11-19 11:26:28 +0100

    tasks 8 and 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleClick);
+function handleClick(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (email.length === 0 || password.length === 0) {
+    alert("All fields must be filled!");
+    return;
+  }
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const buttonChangeColor = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+buttonChangeColor.addEventListener("click", ChangeColor);
+
+function ChangeColor(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}
